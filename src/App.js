695617e2dbe3fe75d9438dd6da9cd2da042b367e
s.a <appurupei64@gmail.com>
2022-12-19 22:22:52 +0900

    p

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { Fragment } from 'react';
 import './App.css';
+import Modal from "./componets/Modal";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Fragment>
+    <div className='p10 text-center'>
+      <h1 className="text-3xl md-5">
+        Crate popup
+      </h1>
+      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5">Text Modal
+      </button>
+      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5">Vide Modal
+      </button>
+      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5">Form Modal
+      </button>
     </div>
+    <Modal />
+    </Fragment>
   );
 }
 
