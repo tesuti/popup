3e5b9411116a2413583137b9598bef19210003e1
tesuti <91707110+tesuti@users.noreply.github.com>
2022-12-20 15:51:30 +0900

    Update Modal.jsx

--- src/componets/Modal.jsx
+++ src/componets/Modal.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
-const Modal = ({is}) => {
+const Modal = ({isVisible, onClose, children}) => {
+  if( !isVisible ) return null;
+
+  const handleClose = (e) =>{
+    if(e.target.id == 'wrapper') onClose();
+  }
+
   return (
-    <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center'>
+    <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center' onClick={handleClose}>
       <div className='w-[600px] flex flex-col'>
-        <button className="text-white text-xl place-self-end">x</button>
+        <button className="text-white text-xl place-self-end" onClick={() => onClose()}>x</button>
         <div className="bg-white p-2 rounded">
-          Modal
+          {children}
         </div>
       </div>
     </div>
